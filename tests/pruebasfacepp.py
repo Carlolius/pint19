f40ec724b4973e30dbe4cd3531df51e1ad52651d
Carlos <carlos.manoso@udc.es>
2018-12-03 21:41:48 +0100

    Mejora del código para obtener las emociones de la api de face++

--- tests/pruebasfacepp.py
+++ tests/pruebasfacepp.py
@@ -4,16 +4,23 @@ key = "GHXsh3Reqt-FKu5Mbjw9NtUXeG-TUsrM"
 secret = "HvD3fbbVWiL3aquA6pDvO-GjIa9nYQ-i"
 image = {}  # inicialización de la variable vacía
 # parameters
-image = 'https://upload.wikimedia.org/wikipedia/commons/d/d7/Mariano_Rajoy_in_2018.jpg'
+image_file = './prueba.jpg'
+image_url = 'https://t1.uc.ltmcdn.com/images/8/5/3/img_como_usar_el_aceite_de_arbol_de_te_en_la_cara_43358_600.jpg'
+image_type = 'url'
 attributes = 'emotion'
 
 data = {
         'api_key': key,
         'api_secret': secret,
-        'image_url': image,
+        'image_url': image_url,
         'return_attributes': attributes
         }
 
-a = requests.post(http_url, params=data)
+if image_type == 'file':
+        image = {'image_file': open(image_file, 'rb')}
+else:
+        data['image_url'] = image_url
+
+a = requests.post(http_url, params=data, files=image)  # no es necesario pasar el parámetro files
 print(a.status_code, a.reason)
 print(a.text)
