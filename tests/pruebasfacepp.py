4dc0fc5fe817fc034688d0429b503d2ee5fe9a54
Carlos <carlos.manoso@udc.es>
2018-12-03 18:31:45 +0100

    Mejora del código para obtener las emociones de la api de face++

--- tests/pruebasfacepp.py
+++ tests/pruebasfacepp.py
@@ -2,6 +2,7 @@ import requests
 http_url = 'https://api-us.faceplusplus.com/facepp/v3/detect'
 key = "GHXsh3Reqt-FKu5Mbjw9NtUXeG-TUsrM"
 secret = "HvD3fbbVWiL3aquA6pDvO-GjIa9nYQ-i"
+image = {}  # inicialización de la variable vacía
 # parameters
 image = 'https://upload.wikimedia.org/wikipedia/commons/d/d7/Mariano_Rajoy_in_2018.jpg'
 attributes = 'emotion'
