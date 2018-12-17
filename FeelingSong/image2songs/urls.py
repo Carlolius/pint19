83d21d171a3a59c1cc943c9a3fe78cd676a19cfe
Javier <javier.rey.lorenzo@udc.es>
2018-12-17 17:40:28 +0100

    modificado urls.py y views.py

--- FeelingSong/image2songs/urls.py
+++ FeelingSong/image2songs/urls.py
@@ -4,5 +4,7 @@ from image2songs import views
 
 urlpatterns=[
 	url(r'^$', views.index, name='index'),
-	url(r'^(?P<userData_id>\d+)/$',views.detail,name='detail'),
+	# url(r'^(?P<userData_id>\d+)/$',views.detail,name='detail'),
+	url('base', views.base, name='base'),
+	url('spotify_access', views.spotify_access, name='access')
 ]
--- FeelingSong/image2songs/views.py
+++ FeelingSong/image2songs/views.py
@@ -9,8 +9,16 @@ def index(request):
     context={'userData_list':userData_list}
     return render(request,'image2songs/index.html', context)
 
+"""
 def detail(request,userData_id):
     f=UserData.objects.get(id=userData_id)
     image2songs_list=f.feeling_set.all()
     context={'f':f,'image2songs_list':image2songs_list}
-    return render(request,'image2songs/userData_detail.html', context)
+    return render(request,'image2songs/userData_detail.html', context) 
+"""
+
+def base(request):
+    return render(request,'image2songs/base.html')
+
+def spotify_access(request):
+    return render(request,'image2songs/spotify_access.html')
