471d9db340ab66df21bbec7b77c387f7cf5e06c1
Noé Vila <noe.vila@udc.es>
2018-12-26 18:39:34 +0100

    Eliminado SpotifyAccess y añadida plantilla register

--- FeelingSong/image2songs/urls.py
+++ FeelingSong/image2songs/urls.py
@@ -2,9 +2,9 @@
 from django.conf.urls import include, url
 from image2songs import views
 
-urlpatterns=[
-	url(r'^$', views.index, name='index'),
-	# url(r'^(?P<userData_id>\d+)/$',views.detail,name='detail'),
-	url('base', views.base, name='base'),
-	url('spotify_access', views.spotify_access, name='access')
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    # url(r'^(?P<userData_id>\d+)/$',views.detail,name='detail'),
+    url('base', views.base, name='base'),
+    url('register', views.register, name='register'),
 ]
--- FeelingSong/image2songs/views.py
+++ FeelingSong/image2songs/views.py
@@ -4,21 +4,14 @@ from image2songs.models import UserData, Feeling
 
 # Create your views here.
 
-def index(request):
-    userData_list=UserData.objects.all()
-    context={'userData_list':userData_list}
-    return render(request,'image2songs/index.html', context)
 
-"""
-def detail(request,userData_id):
-    f=UserData.objects.get(id=userData_id)
-    image2songs_list=f.feeling_set.all()
-    context={'f':f,'image2songs_list':image2songs_list}
-    return render(request,'image2songs/userData_detail.html', context) 
-"""
+def index(request):
+    return render(request, 'image2songs/index.html')
+
 
 def base(request):
-    return render(request,'image2songs/base.html')
+    return render(request, 'image2songs/base.html')
 
-def spotify_access(request):
-    return render(request,'image2songs/spotify_access.html')
+
+def register(request):
+    return render(request, 'image2songs/register.html')
