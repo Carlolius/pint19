5412236178cf388a6ff92ef4965a2a3f6dd07321
Noé Vila <noe.vila@udc.es>
2018-12-27 20:43:08 +0100

    Login con usuarios de Django y base.css arreglada

--- FeelingSong/image2songs/models.py
+++ FeelingSong/image2songs/models.py
@@ -2,25 +2,26 @@ from django.db import models
 
 # Create your models here.
 
+
 class UserData(models.Model):
-    email=models.CharField(max_length=30)
-    user=models.CharField(max_length=50)
-    name=models.CharField(max_length=20)
-    token_s=models.CharField(max_length=50)
+    email = models.CharField(max_length=30)
+    user = models.CharField(max_length=50)
+    name = models.CharField(max_length=20)
+    token_s = models.CharField(max_length=50)
 
     def __unicode__(self):
         return self.name
 
 
 class Feeling(models.Model):
-        email=models.ForeignKey(UserData,on_delete=models.CASCADE)
-        happiness=models.IntegerField()
-        anger=models.IntegerField()
-        neutral=models.IntegerField()
-        sadness=models.IntegerField()
-        fear=models.IntegerField()
-        surprise=models.IntegerField()
-        disgust=models.IntegerField()
+    email = models.ForeignKey(UserData, on_delete=models.CASCADE)
+    happiness = models.IntegerField()
+    anger = models.IntegerField()
+    neutral = models.IntegerField()
+    sadness = models.IntegerField()
+    fear = models.IntegerField()
+    surprise = models.IntegerField()
+    disgust = models.IntegerField()
 
-        def __unicode__(self):
-            return self.email
+    def __unicode__(self):
+        return self.email
--- FeelingSong/image2songs/urls.py
+++ FeelingSong/image2songs/urls.py
@@ -1,6 +1,8 @@
 
 from django.conf.urls import include, url
 from image2songs import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
@@ -8,5 +10,6 @@ urlpatterns = [
     url('base', views.base, name='base'),
     url('register', views.register, name='register'),
     url('webcam', views.webcam, name='webcam'),
-    url('historial', views.historial, name='historial'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name="image2songs/login.html"), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(template_name="image2songs/base.html"), name='logout'),
 ]
